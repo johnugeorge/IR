3a69de4549c292ef4f6f03068aa71e0f4fec9487
Johnu George <johnugeorge109@gmail.com>
2013-02-04 00:36:30 -0500

    Adding first Part

---
+++ HW1/boolean_retrieval.py
@@ -0,0 +1,64 @@
+import os
+import re
+from collections import defaultdict
+
+new_hash=defaultdict(set)
+
+def build_index():
+	dir='books'
+	filenames = os.listdir(dir)
+	for filename in filenames:
+	        rel_path=os.path.join(dir, filename)
+		match=re.search(r'([\d]+).txt', filename)
+                if match:
+			doc_Id= match.group(1)
+		f = open(rel_path, 'rU')
+		#print filename,doc_Id
+		buf=f.read()
+                tokens=re.findall(r"[\w]+", buf)
+		add_Inverted_Index(doc_Id,tokens)
+		f.close()
+	print " Total Keys ",len(new_hash)
+	
+def add_Inverted_Index(doc_Id,tokens):
+	for token in tokens:
+		token=token.lower()
+		new_hash[token].add(doc_Id)
+	      #  new_list=[]
+		#if not token in new_hash:
+		#	new_list.append(doc_Id)
+		#else:
+		#	new_list=new_hash[token]
+		#	new_list.append(doc_Id)
+
+		#new_hash[token] =new_list
+
+def searchfn():
+	while(1):
+		search_string=raw_input('Enter your search string here: ')
+	        search_list=search_string.split()
+	        print search_string,search_list
+	        new_list=[]
+	        result_found=0
+	        for elem in search_list:
+			if elem in new_hash:
+				result_found=1;
+			        new_list.append(new_hash[elem])
+		        else:
+			        result_found=0
+			        break
+                if result_found:
+		        result =set(new_list[0]).intersection(*new_list[1:])
+		        print result
+	        else:
+		        print "sorry no match :("
+
+
+def main():
+	print "Loading Index"
+	build_index()
+	searchfn()
+
+
+if __name__ == '__main__':
+	main()
