7376b77354b87959b14130202275853ea9fe2dbb
Johnu George <johnugeorge109@gmail.com>
2013-04-09 02:40:19 -0400

    Adding F1 measure

--- HW3/classify.py
+++ HW3/classify.py
@@ -16,11 +16,13 @@ testing_dict=defaultdict(innerdict)
 doc_id_to_tokens=defaultdict(list)
 cluster_set=defaultdict(list)
 cluster_set_to_doc_id=defaultdict(list)
+testing_cluster_to_doc_id=defaultdict(list)
 resultSet=defaultdict(innerdict)
 class_prob=defaultdict(float)
 training_cluster_count=0
 training_doc_count=0.0
 testing_set_cluster=defaultdict(int)
+predicted_set_cluster=defaultdict(int)
 testing_cluster_count=0
 testing_doc_count=0.0
 set_of_tokens=set()
@@ -59,7 +61,7 @@ def loadQueries(fileloc):
 	print "Total Vocabulary",len(set_of_tokens)
 	total_vocab_tokens=len(set_of_tokens)
 	calculate_class_prob()
-	print "Class Prob",class_prob
+	#print "Class Prob",class_prob
 
 							       
 def loadTestset(fileloc):
@@ -90,6 +92,7 @@ def calculate_class_prob():
 
 def create_testing_set(tokens,doc_id,cluster_id):
 	testing_set_cluster[doc_id]=cluster_id
+	testing_cluster_to_doc_id[cluster_id].append(doc_id)
 	for token in tokens:
 		token=token.lower()
 		testing_dict[doc_id][token]+=1
@@ -126,7 +129,7 @@ def calculate_naive_classifier():
 			for token in testing_dict[doc]:
 				val=calculate_token_prob(token,cluster)
 				resultSet[doc][cluster] += log_val(val)
-	print resultSet
+	#print resultSet
 	print len(resultSet)
 
 def calculate_accuracy():
@@ -137,13 +140,120 @@ def calculate_accuracy():
 			maxSet[cluster]=resultSet[doc][cluster]
 		results=[(key,val) for key, val in sorted(maxSet.iteritems(), key=lambda (k,v): (v,k))]
 		cluster_id=results[-1][0]
+		predicted_set_cluster[doc]=cluster_id;
 		if testing_set_cluster[doc] == cluster_id:
-			match+=1
+			match +=1
 	print " Final Results match",match
 	print "Accuracy ",match/len(testing_set_cluster)
 
+def calculate_micro_F1_values():
+	true_positives=0.0
+	true_negatives=0.0
+	false_positives=0.0
+	false_negatives=0.0
+	maxSet=defaultdict(float)
+	'''
+	testing_set_cluster.clear()
+	testing_cluster_to_doc_id.clear()
+	predicted_set_cluster.clear()
+	testing_set_cluster[0]=0
+	testing_set_cluster[1]=0
+	testing_set_cluster[2]=0
+	testing_set_cluster[3]=0
+	testing_set_cluster[4]=0
+	testing_set_cluster[5]=0
+	testing_set_cluster[6]=0
+	testing_set_cluster[7]=0
+	testing_set_cluster[8]=1
+	testing_set_cluster[9]=1
+	testing_set_cluster[10]=1
+	testing_set_cluster[11]=1
+	testing_set_cluster[12]=1
+	testing_set_cluster[13]=1
+	testing_set_cluster[14]=2
+	testing_set_cluster[15]=2
+	testing_set_cluster[16]=2
+	testing_set_cluster[17]=2
+	testing_set_cluster[18]=2
+	testing_set_cluster[19]=2
+	testing_set_cluster[20]=2
+	testing_set_cluster[21]=2
+	testing_set_cluster[22]=2
+	testing_set_cluster[23]=2
+	testing_set_cluster[24]=2
+	testing_set_cluster[25]=2
+	testing_set_cluster[26]=2
+	predicted_set_cluster[0]=0
+	predicted_set_cluster[1]=0
+	predicted_set_cluster[2]=0
+	predicted_set_cluster[3]=0
+	predicted_set_cluster[4]=0
+	predicted_set_cluster[5]=1
+	predicted_set_cluster[6]=1
+	predicted_set_cluster[7]=1
+	predicted_set_cluster[8]=0
+	predicted_set_cluster[9]=0
+	predicted_set_cluster[10]=1
+	predicted_set_cluster[11]=1
+	predicted_set_cluster[12]=1
+	predicted_set_cluster[13]=2
+	predicted_set_cluster[14]=1
+	predicted_set_cluster[15]=1
+	predicted_set_cluster[16]=2
+	predicted_set_cluster[17]=2
+	predicted_set_cluster[18]=2
+	predicted_set_cluster[19]=2
+	predicted_set_cluster[20]=2
+	predicted_set_cluster[21]=2
+	predicted_set_cluster[22]=2
+	predicted_set_cluster[23]=2
+	predicted_set_cluster[24]=2
+	predicted_set_cluster[25]=2
+	predicted_set_cluster[26]=2
+
+  	testing_cluster_to_doc_id[0]=[0,1,2,3,4,5,6,7]	
+  	testing_cluster_to_doc_id[1]=[8,9,10,11,12,13]
+  	testing_cluster_to_doc_id[2]=[14,15,16,17,18,19,20,21,22,23,24,25,26]
+	'''
+	#print testing_set_cluster
+	#print
+	#print predicted_set_cluster
+	print len(testing_set_cluster)
+	print len(predicted_set_cluster)
+	for cluster in testing_cluster_to_doc_id:
+		print "size is",len(testing_cluster_to_doc_id[cluster])," for cluster",cluster
+		for doc in testing_set_cluster:
+			actual=testing_set_cluster[doc]
+			predicted=predicted_set_cluster[doc]
+			#print "actual ",actual,"predicted ",predicted," for doc ",doc
+			if cluster == actual and predicted == cluster:
+				#print "true_positive"
+				true_positives += 1
+			elif cluster != actual and predicted == cluster:
+				#print "false_positive"
+				false_positives += 1
+			elif cluster == actual and predicted != cluster:
+				#print "false_negative"
+				false_negatives += 1
+			elif cluster != actual and predicted != cluster:
+				#print "true_negative"
+				true_negatives += 1
+	print "true_postives",true_positives
+	print "true_negatives",true_negatives
+	print "false_postives",false_positives
+	print "false_negatives",false_negatives
+	recall=true_positives/(true_positives + false_negatives)
+	precision=true_positives/(true_positives + false_positives)
+	f1_values=2*recall*precision/(recall+precision)
+	print f1_values
+
+
+
+			
+
 def main():
-	        print "Loading Training Set from Local file"
+	        #'''
+		print "Loading Training Set from Local file"
 		print
 	        signal.signal(signal.SIGINT, handler)
 		fileloc="entertainment.json"
@@ -162,6 +272,9 @@ def main():
 		loadTestset(fileloc)
 		calculate_naive_classifier()
 		calculate_accuracy()
+		#'''
+		
+		calculate_micro_F1_values()
 
 if __name__ == '__main__':
 	main()
